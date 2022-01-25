c0196dc5ccb3ebca6699e1a4194b27be359c5a0c
Dmitry Volkov <dmitrij_volkov_2014@mail.ru>
2022-01-25 15:01:27 +0600

    создание настройка роутера и создание страниц

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./Auth.module.scss";
+
+class Auth extends Component {
+  render() {
+    return <h1>Auth</h1>;
+  }
+}
+
+export default Auth;
---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import classes from "./QuizCreator.module.scss";
+
+class QuizCreator extends Component {
+  render() {
+    return <h1>QuizCreator</h1>;
+  }
+}
+
+export default QuizCreator;
